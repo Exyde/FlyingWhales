52f4b45b712816d3cd21e4d40f08f17eb4ef49a8
Exyde <54008677+Exyde@users.noreply.github.com>
2022-06-05 15:49:47 +0200

    Audio Manager Fixes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -10,36 +10,43 @@ public class AudioManager : MonoBehaviour
     public AudioSource source4;
     public AudioSource source5;
 
-    public AudioClip music1;
-    public AudioClip music2;
-    public AudioClip music3;
-    public AudioClip music4;
-    public AudioClip music5;
+    public int _transitionDuration;
+    public int _localPollution;
+
+    // public AudioClip music1;
+    // public AudioClip music2;
+    // public AudioClip music3;
+    // public AudioClip music4;
+    // public AudioClip music5;
     JaugeSystem _jaugeSystem;
 
     IEnumerator volumeTransition(AudioSource track1, AudioSource track2)
     {
-        for (float i = 0; i == 10; i += 1)
+        Debug.Log("Lerping sound");
+        for (float i = 0; i <= 1; i += 0.1f)
         {
-            track1.volume -= 1;
-            track2.volume += 1;
-            yield return new WaitForSeconds(0.2f);
+            Debug.Log("i : "  + i);
+            track1.volume -= 0.1f;
+            track2.volume += 0.1f;
+            yield return new WaitForSeconds(_transitionDuration / 10);
         }
     }
 
     void Start()
     {
-        source1 = GetComponent<AudioSource>();
-        source2 = GetComponent<AudioSource>();
-        source3 = GetComponent<AudioSource>();
-        source4 = GetComponent<AudioSource>();
-        source5 = GetComponent<AudioSource>();
+        // source1 = GetComponent<AudioSource>();
+        // source2 = GetComponent<AudioSource>();
+        // source3 = GetComponent<AudioSource>();
+        // source4 = GetComponent<AudioSource>();
+        // source5 = GetComponent<AudioSource>();
 
         _jaugeSystem = GameObject.FindObjectOfType<JaugeSystem>();
+
         source2.volume = 0;
         source3.volume = 0;
         source4.volume = 0;
         source5.volume = 0;
+
         source1.Play();
         source2.Play();
         source3.Play();
@@ -51,22 +58,24 @@ public class AudioManager : MonoBehaviour
     void Update()
     { 
 
-        if (_jaugeSystem.Pollution >= 30)
+        _localPollution = _jaugeSystem.JaugePollution;
+
+        if (_localPollution >= 300)
         {
             StartCoroutine(volumeTransition(source1, source2));
         }
 
-        if (_jaugeSystem.Pollution >= 50)
+        if (_localPollution >= 500)
         {
             StartCoroutine(volumeTransition(source2, source3));
         }
 
-        if (_jaugeSystem.Pollution >= 70)
+        if (_localPollution >= 700)
         {
             StartCoroutine(volumeTransition(source3, source4));
         }
 
-        if (_jaugeSystem.Pollution >= 90)
+        if (_localPollution >= 900)
         {
             StartCoroutine(volumeTransition(source4, source5));
         }

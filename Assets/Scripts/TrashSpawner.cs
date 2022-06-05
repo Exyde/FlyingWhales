bc25ef0884a6070e92e85ebc7814a03e14aff96e
Exyde <54008677+Exyde@users.noreply.github.com>
2022-06-05 17:43:56 +0200

    Trash Spawner

--- Assets/Scripts/TrashSpawner.cs
+++ Assets/Scripts/TrashSpawner.cs
@@ -6,6 +6,20 @@ public class TrashSpawner : MonoBehaviour
 {
     public List<GameObject> _trashPrefabs;
 
+    public List<Transform> _trashSpawners;
     private JaugeSystem _jaugeSystem;
+    int _pollutionPercent;
+
+
+    private void Awake()
+    {
+        _jaugeSystem = FindObjectOfType<JaugeSystem>();
+    }
+
+
+    private void Update()
+    {
+        _pollutionPercent = _jaugeSystem.JaugePollution / 10;
+    }
     
 }
